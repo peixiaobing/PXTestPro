689f26bffc4e863dd96f87c922f118b817b069b2
裴小兵 <1146611712@qq.com>
2021-07-04 10:15:59 +0800

    提交1

---
+++ 222333/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  222333
+//
+//  Created by 裴小兵 on 2021/4/8.
+//  Copyright © 2021 裴小兵. All rights reserved.
+//  100
+//   239
+//
+import Foundation
+
+print("Hello, World!")
+
